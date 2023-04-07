d6ab63125885cc5718789cc2f3fec4309ed7de88
Mengyi YOENG <YOENG.Mengyi@ABABANK.COM>
2023-04-07 16:42:42 +0700

    aa

--- components/ScanQR.vue
+++ components/ScanQR.vue
@@ -27,7 +27,7 @@ export default {
     data() {
         return {
             result: '',
-            isOpen: false,
+            isOpen: true,
             isLink: false,
             link: '',
         };
