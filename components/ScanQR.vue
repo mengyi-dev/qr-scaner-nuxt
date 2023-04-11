76fa19a8773dc56882efa816f6175a91057a6b8c
Mengyi YOENG <YOENG.Mengyi@ABABANK.COM>
2023-04-11 10:04:35 +0700

    add copy text

--- components/ScanQR.vue
+++ components/ScanQR.vue
@@ -20,8 +20,18 @@
             </div>
             <a :href="result" class="break-words truncate">{{result}} {{ isValidURL }}</a>
           </div>
-          <div v-else class="w-full bg-white h-auto p-5 rounded-md break-words">
+          <div v-else class="w-full bg-white h-auto p-5 rounded-md break-all relative">
+            <span>
               {{ result }}
+            </span>
+            <div @click="copyResult" ref="copyButton" :data-clipboard-text="result" class="absolute top-0 right-0 cursor-pointer">
+              <svg v-if="isCopied" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-green-600">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M10.125 2.25h-4.5c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125v-9M10.125 2.25h.375a9 9 0 019 9v.375M10.125 2.25A3.375 3.375 0 0113.5 5.625v1.5c0 .621.504 1.125 1.125 1.125h1.5a3.375 3.375 0 013.375 3.375M9 15l2.25 2.25L15 12" />
+              </svg>
+              <svg v-else xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 01-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 011.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 00-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 01-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5a3.375 3.375 0 00-3.375-3.375H9.75" />
+              </svg>
+            </div>
           </div>
         </div>
       </div>
@@ -31,6 +41,7 @@
 </template>
 
 <script>
+import Clipboard from 'clipboard';
 export default {
     name: "ScanQR",
     data() {
@@ -38,6 +49,7 @@ export default {
             result: null,
             isOpen: false,
             link: '',
+            isCopied: false,
         };
     },
     methods: {
@@ -50,12 +62,25 @@ export default {
         close(){
           this.isOpen = false;
           this.result = null;
+        },
+        copyResult(){
+          // Get the button element by ref
+          const button = this.$refs.copyButton;
+          
+          // Create a new Clipboard instance attached to the button element
+          const clipboard = new Clipboard(button);
+          
+          // Log the text that has been copied to the clipboard
+          clipboard.on('success', (e) => {
+            console.log('Copied:', e.text);
+          });
+          this.isCopied = true
         }
     },
     computed: {
       isValidURL(){
-        return this.result.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g) !== null
+        return typeof this.result === 'string' ? this.result.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g) !== null : false;
       }
-    }
+    },
 }
 </script>
